f28bf6cd14a87da0f06abcfc358f2e6f3db7fbb6
TechnoFlask <technoflask03@gmail.com>
2024-01-26 21:09:52 +0530

    commit

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -8,12 +8,12 @@ static const unsigned int snap = 32;    /* snap pixel */
 // inner-hor -6
 // outer-ver -10
 // outer-hor +14
-static const unsigned int gappih = 15; /* horiz inner gap between windows */
-static const unsigned int gappiv = 15; /* vert inner gap between windows */
+static const unsigned int gappih = 8; /* horiz inner gap between windows */
+static const unsigned int gappiv = 8; /* vert inner gap between windows */
 static const unsigned int gappoh =
-    25; /* horiz outer gap between windows and screen edge */
+    30; /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov =
-    25; /* vert outer gap between windows and screen edge */
+    30; /* vert outer gap between windows and screen edge */
 static int smartgaps =
     0; /* 1 means no outer gap when there is only one window */
 
