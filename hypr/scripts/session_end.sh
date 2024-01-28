d85db6c35bd4108941ec215649be301169c3bb7b
TechnoFlask <technoflask03@gmail.com>
2024-01-28 23:53:20 +0530

    Some minor changes in: waybar session start script neofetch hyprland end script

--- hypr/scripts/session_end.sh
+++ hypr/scripts/session_end.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-choice=$(printf "Shutdown\nReboot\nLogout" | rofi -dmenu -i)
+choice=$(printf "Shutdown\nReboot\nLogout" | rofi -dmenu -p "Session end" -i)
 
 case "$choice" in
   "Shutdown") shutdown now ;;
